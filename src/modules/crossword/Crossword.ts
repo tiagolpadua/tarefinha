c022b3a36b26bce668160fe75256d832d9a8a4be
Tiago Lage Payne de Pádua <tiago@Mcbooks-MacBook-Air.local>
2018-04-03 19:24:36 -0300

    tests

--- src/modules/crossword/Crossword.ts
+++ src/modules/crossword/Crossword.ts
@@ -1,15 +1,41 @@
+declare var _: {};
+
 interface ITerm {
     word: string;
     image: string;
 }
 
 interface IDimenson {
-    x: number;
-    y: number;
+    width: number;
+    height: number;
 }
 
 class Crossword {
-    constructor(private terms: ITerm[], private dimension: IDimenson) {
-        
-    }
+    // private map: string[][];
+
+    // constructor(private terms: ITerm[], private dimensions: IDimenson) {
+    //     this.map = getEmptyMap(this.dimensions);
+    //     // Math.random() * terms.length;
+    //     console.log(this.map);
+    //     console.log(this.terms);
+    //     console.log(this.dimensions);
+    // }
+
 }
+
+export default function foo() {
+    return 'foo';
+}
+
+export function getEmptyMap(dimensions: IDimenson): string[][] {
+    const m: string[][] = [];
+    for (let x = 0; x < dimensions.width; x ++) {
+        m[x] = [];
+        for (let y = 0; y < dimensions.height; y ++) {
+            m[x].push('');
+        }
+    }
+    return m;
+}
+
+// export default Crossword;
